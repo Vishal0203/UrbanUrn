0fdf8e460b92124bbf8ea34802e657df5ce2d753
Vishal Sharma <vish.sharma0203@gmail.com>
2015-09-30 01:31:03 +0530

    made changes to the uuid and json datatypes, added enums

--- Urn/models.py
+++ Urn/models.py
@@ -8,13 +8,31 @@
 # Also note: You'll have to insert the output of 'django-admin sqlcustom [app_label]'
 # into your database.
 from __future__ import unicode_literals
-
+from django_enumfield import enum
+from jsonfield import JSONField
 from django.db import models
+
+
+class Role(enum.Enum):
+    owner = 0
+    admin = 1
+    member = 2
+
+
+class Rating(enum.Enum):
+    pass
+
+
+class Status(enum.Enum):
+    active = 0
+    online = 1
+    offline = 2
+    deleted = 3
 
 
 class Addresses(models.Model):
     address_id = models.AutoField(primary_key=True)
-    address_guid = models.TextField(unique=True)  # This field type is a guess.
+    address_guid = models.UUIDField(unique=True)
     is_default = models.NullBooleanField()
     street_1 = models.CharField(max_length=50, blank=True, null=True)
     street_2 = models.CharField(max_length=50, blank=True, null=True)
@@ -32,72 +50,6 @@ class Addresses(models.Model):
         db_table = 'addresses'
 
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=80)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    group = models.ForeignKey(AuthGroup)
-    permission = models.ForeignKey('AuthPermission')
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group_id', 'permission_id'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType')
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type_id', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=30)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    user = models.ForeignKey(AuthUser)
-    group = models.ForeignKey(AuthGroup)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user_id', 'group_id'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    user = models.ForeignKey(AuthUser)
-    permission = models.ForeignKey(AuthPermission)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user_id', 'permission_id'),)
-
-
 class BusinessAddresses(models.Model):
     business = models.ForeignKey('Businesses')
     address = models.ForeignKey(Addresses)
@@ -113,7 +65,7 @@ class BusinessAddresses(models.Model):
 class BusinessUsers(models.Model):
     business = models.ForeignKey('Businesses')
     user = models.ForeignKey('Users')
-    role = models.TextField(blank=True, null=True)  # This field type is a guess.
+    role = enum.EnumField(Role)
     created_on = models.DateTimeField(blank=True, null=True)
     updated_on = models.DateTimeField(blank=True, null=True)
 
@@ -125,7 +77,7 @@ class BusinessUsers(models.Model):
 
 class Businesses(models.Model):
     business_id = models.AutoField(primary_key=True)
-    business_guid = models.TextField(unique=True)  # This field type is a guess.
+    business_guid = models.UUIDField(unique=True)
     name = models.CharField(max_length=50, blank=True, null=True)
     category = models.CharField(max_length=20, blank=True, null=True)
     description = models.CharField(max_length=1024, blank=True, null=True)
@@ -141,10 +93,10 @@ class Businesses(models.Model):
 
 class CartItems(models.Model):
     cart_item_id = models.AutoField(primary_key=True)
-    cart_item_guid = models.TextField(unique=True)  # This field type is a guess.
+    cart_item_guid = models.UUIDField(unique=True)
     product = models.ForeignKey('Products')
     user = models.ForeignKey('Users')
-    product_data = models.TextField(blank=True, null=True)  # This field type is a guess.
+    product_data = JSONField(blank=True, null=True)
     created_on = models.TimeField(blank=True, null=True)
     updated_on = models.TimeField(blank=True, null=True)
 
@@ -155,7 +107,7 @@ class CartItems(models.Model):
 
 class Coupons(models.Model):
     coupon_id = models.AutoField(primary_key=True)
-    coupon_guid = models.TextField(unique=True)  # This field type is a guess.
+    coupon_guid = models.UUIDField(unique=True)
     code = models.CharField(max_length=30)
     discount_value = models.IntegerField()
     is_percentage = models.NullBooleanField()
@@ -169,7 +121,7 @@ class Coupons(models.Model):
 
 class Discounts(models.Model):
     discount_id = models.AutoField(primary_key=True)
-    discount_guid = models.TextField(unique=True)  # This field type is a guess.
+    discount_guid = models.UUIDField(unique=True)
     product = models.ForeignKey('Products', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     start_time = models.DateTimeField(blank=True, null=True)
@@ -188,50 +140,6 @@ class Discounts(models.Model):
         db_table = 'discounts'
 
 
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.SmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', blank=True, null=True)
-    user = models.ForeignKey(AuthUser)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
-
-
 class EntityKeywords(models.Model):
     entity_keyword_id = models.AutoField(primary_key=True)
     keyword = models.ForeignKey('Keywords')
@@ -247,7 +155,7 @@ class EntityKeywords(models.Model):
 
 class Keywords(models.Model):
     keyword_id = models.AutoField(primary_key=True)
-    keyword_guid = models.TextField(unique=True)  # This field type is a guess.
+    keyword_guid = models.UUIDField(unique=True)
     name = models.CharField(max_length=30, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     created_by = models.ForeignKey('Users', db_column='created_by')
@@ -268,7 +176,7 @@ class OrderDetails(models.Model):
     discount = models.ForeignKey(Discounts, blank=True, null=True)
     final_cost = models.FloatField(blank=True, null=True)
     status = models.NullBooleanField()
-    product_data = models.TextField(blank=True, null=True)  # This field type is a guess.
+    product_data = JSONField(blank=True, null=True)
     created_on = models.DateTimeField(blank=True, null=True)
     updated_on = models.DateTimeField(blank=True, null=True)
 
@@ -279,7 +187,7 @@ class OrderDetails(models.Model):
 
 class Orders(models.Model):
     order_id = models.AutoField(primary_key=True)
-    order_guid = models.TextField(unique=True)  # This field type is a guess.
+    order_guid = models.UUIDField(unique=True)
     user = models.ForeignKey('Users')
     address_id = models.IntegerField()
     coupon = models.ForeignKey(Coupons, blank=True, null=True)
@@ -308,12 +216,12 @@ class ProductImages(models.Model):
 
 class Products(models.Model):
     product_id = models.AutoField(primary_key=True)
-    product_guid = models.TextField(unique=True)  # This field type is a guess.
+    product_guid = models.UUIDField(unique=True)
     name = models.CharField(max_length=50, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     status = models.NullBooleanField()
     price = models.FloatField()
-    product_data = models.TextField(blank=True, null=True)  # This field type is a guess.
+    product_data = JSONField(blank=True, null=True)
     business = models.ForeignKey(Businesses)
     sku = models.ForeignKey('Sku')
     created_by = models.ForeignKey('Users', db_column='created_by', blank=True, null=True)
@@ -328,8 +236,8 @@ class Products(models.Model):
 
 class Reviews(models.Model):
     review_id = models.AutoField(primary_key=True)
-    review_guid = models.TextField(unique=True)  # This field type is a guess.
-    rating = models.TextField()  # This field type is a guess.
+    review_guid = models.UUIDField(unique=True)
+    rating = models.TextField()  # ToDo This field type is a guess. doubt on enum, yet to decide
     review_detail = models.TextField(blank=True, null=True)
     user = models.ForeignKey('Users')
     business = models.ForeignKey(Businesses, blank=True, null=True)
@@ -344,7 +252,7 @@ class Reviews(models.Model):
 
 class Sku(models.Model):
     sku_id = models.AutoField(primary_key=True)
-    sku_guid = models.TextField(unique=True)  # This field type is a guess.
+    sku_guid = models.UUIDField(unique=True)
     name = models.CharField(max_length=30, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     status = models.NullBooleanField()
@@ -387,7 +295,7 @@ class UserAddresses(models.Model):
 
 class Users(models.Model):
     user_id = models.AutoField(primary_key=True)
-    user_guid = models.TextField(unique=True)  # This field type is a guess.
+    user_guid = models.UUIDField(unique=True)
     email = models.CharField(unique=True, max_length=120)
     username = models.CharField(unique=True, max_length=20)
     password = models.CharField(max_length=30)
@@ -396,7 +304,7 @@ class Users(models.Model):
     phone = models.CharField(max_length=12)
     is_business_user = models.NullBooleanField()
     is_superuser = models.NullBooleanField()
-    status = models.TextField()  # This field type is a guess.
+    status = enum.EnumField(Status, default=Status.active)
     last_logged_on = models.DateTimeField(blank=True, null=True)
     push_notification = models.NullBooleanField()
     email_notification = models.NullBooleanField()
@@ -411,10 +319,10 @@ class Users(models.Model):
 
 class Wishlist(models.Model):
     wishlist_id = models.AutoField(primary_key=True)
-    wishlist_guid = models.TextField(unique=True)  # This field type is a guess.
+    wishlist_guid = models.UUIDField(unique=True)
     user = models.ForeignKey(Users, blank=True, null=True)
     product = models.ForeignKey(Products)
-    product_data = models.TextField(blank=True, null=True)  # This field type is a guess.
+    product_data = JSONField(blank=True, null=True)
     created_on = models.DateTimeField(blank=True, null=True)
     updated_on = models.DateTimeField(blank=True, null=True)
 
