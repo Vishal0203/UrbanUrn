f47559f081e1bfdae6ba20a8ae5030a97ba3aaa4
Vishal Sharma <vish.sharma0203@gmail.com>
2015-10-04 07:48:56 +0530

    added login api url

--- UrbanUrn/urls.py
+++ UrbanUrn/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from Urn import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/v1_0/login', views.validate_input_and_authenticate)
 ]
