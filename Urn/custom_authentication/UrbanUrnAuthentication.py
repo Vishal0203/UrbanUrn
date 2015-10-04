253d2cbf65db3f2de87b9588abe10667b01bf359
Vishal Sharma <vish.sharma0203@gmail.com>
2015-10-04 11:12:43 +0530

    removing custom auth model

--- Urn/custom_authentication/UrbanUrnAuthentication.py
+++
@@ -1,8 +0,0 @@
-from Urn.models import Users
-
-
-class UrbanUrnAuthentication(object):
-
-    def authenticate(self, username=None, password=None):
-        user = Users.objects.get(email=username)
-        pass
