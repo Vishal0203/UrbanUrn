212eff4ec15958c796ffa3d007078ca321ffef16
Vishal Sharma <vish.sharma0203@gmail.com>
2015-11-09 15:49:00 +0530

    missed field in cookie rename

--- Urn/decorators/cookie_manager.py
+++ Urn/decorators/cookie_manager.py
@@ -4,10 +4,10 @@ from UrbanUrn import settings
 
 def manage_cookie(original_function):
     def check_or_set_cookie(request_arg):
-        if request_arg.COOKIES.get(settings.ANONYMOUS_SESSION_NAME, None) is None:
+        if request_arg.COOKIES.get(settings.BROWSER_COOKIE_NAME, None) is None:
             session_id = request_arg.session._get_or_create_session_key()
             response = original_function(request_arg)
-            response.set_cookie(settings.ANONYMOUS_SESSION_NAME, session_id)
+            response.set_cookie(settings.BROWSER_COOKIE_NAME, session_id)
         else:
             response = original_function(request_arg)
 
