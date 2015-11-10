7db134166303e95de3beb3c718ee306239928f57
Vishal Sharma <vish.sharma0203@gmail.com>
2015-11-10 12:35:42 +0530

    wish list GET and POST without formatting

---
+++ Urn/schema_validators/wishlist_validator.py
@@ -0,0 +1,12 @@
+wishlist_post = {
+    "type": "object",
+    "properties": {
+        "product_guid": {
+            "type": "string"
+        },
+        "product_data": {
+            "type": "string"
+        }
+    },
+    "required": ["product_guid"]
+}
--- Urn/views/wishlist.py
+++ Urn/views/wishlist.py
@@ -1,6 +1,9 @@
+import json
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
-from Urn.decorators.validators import jwt_validate
+from Urn.decorators.validators import jwt_validate, validate_schema
+from Urn.models import Products, Wishlist, Users
+from Urn.schema_validators.wishlist_validator import wishlist_post
 
 
 @csrf_exempt
@@ -16,12 +19,33 @@ def process_wishlist_api(request):
         return HttpResponseBadRequest("API not found")
 
 
+# ToDo formatting the get call
 def process_wishlist_get(request):
-    pass
+    if request.user.is_staff or request.user.is_superuser:
+        user_guid = request.GET.get("user_guid", None)
+        if user_guid is None:
+            return HttpResponseBadRequest("Expected parameter user_guid")
+        user_info = Users.objects.filter(user_guid=user_guid)
+        if user_info.exists():
+            users_wishlist = user_info.get().wishlist_set.all()
+        else:
+            return HttpResponseBadRequest("No such user exist")
+    else:
+        users_wishlist = request.user.user_profile.wishlist_set.all()
 
 
+@validate_schema(wishlist_post)
 def process_wishlist_post(request):
-    pass
+    request_data = json.loads(request.body.decode())
+    product = Products.objects.filter(product_guid=request_data["product_guid"])
+    if product.exists():
+        product_info = product.get()
+        Wishlist.objects.create(user_id=request.user.user_profile.user_id,
+                                product_id=product_info.product_id,
+                                product_data=json.dumps(request_data["product_data"]))
+        return HttpResponse("Product added to wishlist")
+    else:
+        return HttpResponseBadRequest("No such product")
 
 
 def process_wishlist_put(request):
