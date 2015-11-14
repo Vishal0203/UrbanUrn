fabce505dc62a163a038be7fa723ab411519083d
Vishal Sharma <vish.sharma0203@gmail.com>
2015-11-14 22:56:54 +0530

    discounts skeleton

--- Urn/views/discounts.py
+++ Urn/views/discounts.py
@@ -1,7 +1,31 @@
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
+from Urn.decorators.validators import jwt_validate, check_business_or_super
 
 
 @csrf_exempt
 def process_discount_request(request):
+    if request.method == 'POST':
+        return process_discount_post(request)
+    elif request.method == 'GET':
+        return process_discount_get(request)
+    elif request.method == 'PUT':
+        return process_discount_put(request)
+    else:
+        return HttpResponseBadRequest("API not found")
+
+
+@jwt_validate
+@check_business_or_super
+def process_discount_post(request):
+    pass
+
+
+def process_discount_get(request):
+    pass
+
+
+@jwt_validate
+@check_business_or_super
+def process_discount_put(request):
     pass
