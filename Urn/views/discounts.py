55f04664b6be96281321d53cd0599094659189b5
Vishal Sharma <vish.sharma0203@gmail.com>
2015-11-15 09:17:24 +0530

    discounts POST partial

--- Urn/views/discounts.py
+++ Urn/views/discounts.py
@@ -1,6 +1,9 @@
+import json
+from datetime import datetime
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from Urn.decorators.validators import jwt_validate, check_business_or_super
+from Urn.models import Products, Discounts
 
 
 @csrf_exempt
@@ -18,7 +21,17 @@ def process_discount_request(request):
 @jwt_validate
 @check_business_or_super
 def process_discount_post(request):
-    pass
+    request_data = json.loads(request.body.decode())
+    product = Products.objects.filter(product_guid=request['product_guid'])
+    if product.exists():
+        discount = Discounts.objects.create(product_id=product.get().product_id,
+                                            description=request_data["description"],
+                                            start_time=datetime.strptime(request_data["start_time"],
+                                                                         "%Y-%m-%d %H:%M:%S"),
+                                            end_time=datetime.strptime(request_data["end_time"], "%Y-%m-%d %H:%M:%S"),
+                                            discount_value=request_data["discount_value"],
+                                            is_percentage=request_data["is_percentage"],
+                                            product_quantity=request_data["product_quantity"])
 
 
 def process_discount_get(request):
