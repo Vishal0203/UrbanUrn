29999efcd99ae992c3ed21e14ee3298b6a27873d
gitam-kavya <kaaavya11@gmail.com>
2015-12-05 13:26:07 +0530

    home page implementation

--- Urn/static/angular/app/directives.js
+++ Urn/static/angular/app/directives.js
@@ -73,4 +73,39 @@ angular.module('urn')
                 })
             }
         }
+    })
+    .directive('commonSlider', function() {
+        return {
+            restrict: 'A',
+            scope: {
+                itemCount : '='
+            },
+            link: function(scope, element) {
+                $(element).owlCarousel({
+                    items: parseInt(scope.itemCount), //10 items above 1000px browser width
+                    itemsDesktop: [1024, 4], //5 items between 1024px and 901px
+                    itemsDesktopSmall: [991, 3], // 3 items betweem 900px and 601px
+                    itemsTablet: [600, 2], //2 items between 600 and 0;
+                    itemsMobile: [320, 1],
+                    navigation: true,
+                    navigationText: ["<a class=\"flex-prev\"></a>", "<a class=\"flex-next\"></a>"],
+                    slideSpeed: 500,
+                    pagination: false
+                });
+            }
+        }
+    })
+    .directive('offerSlider', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element) {
+                jQuery(document).ready(function () {
+                   $(element).bxSlider({
+                        mode: 'horizontal',
+                        slideMargin: 3,
+                        auto:true
+                    });
+                });
+            }
+        }
     });
