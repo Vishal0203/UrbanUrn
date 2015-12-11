7eb60d1dabb0398a53cb938e15fe135b6ffdfb72
Vishal Sharma <vish.sharma0203@gmail.com>
2015-12-11 12:40:15 +0530

    missed env file

---
+++ UrbanurnHelper/config/env.py
@@ -0,0 +1,14 @@
+config = {
+    'USERNAME': '<superuser_username>',
+    'PASSWORD': '<superuser_password>',
+    # 'BASE_URL': r'http://localhost:9090',
+    'BASE_URL': '<base_url>',
+    'PRODUCT_KEYS': ['sku_guid', 'name', 'description', 'product_data', 'is_fragile', 'price', 'business_guid'],
+    # 'PRODUCT_IMAGES': r'C:\Users\KH2021\Google Drive\Urban Urn\Images'
+    'PRODUCT_IMAGES': '<uri_for_images>'
+}
+
+excel_config = {
+    # 'PRODUCT_EXCEL': r'C:\Users\KH2021\Google Drive\Urban Urn\Products_table.xlsx'
+    'PRODUCT_EXCEL': '<path_to_excel>'
+}
